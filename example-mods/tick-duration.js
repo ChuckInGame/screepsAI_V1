f223fcb53ad9fe71c0ff31a073579a957ef06175
ChuckInGame <git.mail@wegmann.info>
2019-04-06 16:42:48 +0200

    added simplebot

--- example-mods/tick-duration.js
+++ example-mods/tick-duration.js
@@ -1,7 +1,9 @@
 // Change tick duration to 200 ms
 
-module.exports = function(config) {
-    if(config.engine) {
-        config.engine.mainLoopMinDuration = 200;
+module.exports = function (config)
+{
+    if (config.engine)
+    {
+        config.engine.mainLoopMinDuration = 20;
     }
 };
